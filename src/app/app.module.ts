878b9c88334c2bb206ab18392ce361e1d5dc8d70
Camo <47070087+Camo651@users.noreply.github.com>
2023-03-09 10:51:24 -0500

    scrap and restart... lol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DayCalComponent } from './day-cal/day-cal.component';
-import { TodoCardComponent } from './todo-card/todo-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DayCalComponent,
-    TodoCardComponent
   ],
   imports: [
     BrowserModule
