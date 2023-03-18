2fa5bbfc3b1ab724573819fbaa42515ec263424b
Camo <47070087+Camo651@users.noreply.github.com>
2023-03-18 17:05:58 -0400

    Mockups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
+import { BoardComponent } from './board/board.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TaskComponent,
+    CardComponent,
+    HeaderComponent,
+    BoardComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule
