b0008156308e65159c89cd689980e15e260e8894
Camo <47070087+Camo651@users.noreply.github.com>
2023-03-21 15:51:54 -0400

    More blocking and homepage styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from '../pages/homepage/homepage.component';
 import { DashboardComponent } from '../pages/dashboard/dashboard.component';
+import { CardComponent } from '../components/card/card.component';
+import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { ModalComponent } from '../components/modal/modal.component';
+import { BoardCardComponent } from '../components/board-card/board-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    DashboardComponent
+    DashboardComponent,
+    CardComponent,
+    SidebarComponent,
+    ModalComponent,
+    BoardCardComponent,
   ],
   imports: [
     BrowserModule
